8736c9e410aa016412fd9d6f602f9a1beb0de8b5
vinxgradxv <335040@niuitmo.ru>
2024-11-21 22:53:45 +0300

    initial commit

--- src/main/java/com/example/zuul/ZuulApplication.java
+++ src/main/java/com/example/zuul/ZuulApplication.java
@@ -3,10 +3,7 @@ package com.example.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class ZuulApplication {
 
